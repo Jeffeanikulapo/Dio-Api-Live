049cecb59ba4349e8d85b8db6588149e7139fd22
Jeffeanikulapo <jefe_ss@hotmail.com>
2021-08-10 14:10:29 -0300

    Primeiro commit

---
+++ src/main/java/one/digitalinnovation/personapimaven/PersonapimavenApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.personapimaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonapimavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonapimavenApplication.class, args);
+	}
+
+}
